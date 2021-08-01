743a05a887d534787b8d5d2d3f07f0eefb31aacc
jo <jo@bruelltuete.com>
2021-08-02 00:45:53 +0100

    Various ggate fixes.
    ggate: add unit test for flush support
    
    Add ggated rc script

--- tests/sys/geom/class/gate/ggate_test.sh
+++ tests/sys/geom/class/gate/ggate_test.sh
@@ -4,6 +4,81 @@ PLAINFILES=plainfiles
 PORT=33080
 CONF=gg.exports
 
+atf_test_case ggate_flush cleanup
+ggate_flush_head()
+{
+	atf_set "descr" "a flush on the ggatec side makes it all the way to ggated"
+	atf_set "require.progs" "ggatec ggated gnop"
+	atf_set "require.user" "root"
+	atf_set "timeout" 60
+}
+
+ggate_flush_body()
+{
+	load_ggate
+	us=$(alloc_ggate_dev)
+	md=$(alloc_md)
+	gnop create /dev/$md
+	echo "localhost RW /dev/$md.nop" > $CONF
+	atf_check ggated -p $PORT -F $PIDFILE $CONF
+	atf_check ggatec create -p $PORT -u $us localhost /dev/$md.nop
+
+	ggate_dev=/dev/ggate${us}
+	wait_for_ggate_device ${ggate_dev}
+	# trigger some flushing.
+	atf_check -s exit:0 -e ignore -o ignore zpool create testpool4ggateflush ${ggate_dev}
+
+	numflushes=$(gnop list /dev/$md.nop | grep Flushes | awk '{ print $2 }')
+
+	zpool destroy testpool4ggateflush
+	#gnop destroy /dev/$md.nop
+
+	if [ ${numflushes} -gt "0" ]
+	then
+		atf_pass
+	fi
+
+	atf_fail "No flushes arrived on ggated side"
+}
+
+ggate_flush_cleanup()
+{
+        common_cleanup
+}
+
+atf_test_case ggatec_trim cleanup
+ggatec_trim_head()
+{
+	atf_set "descr" "ggatec survives a trim"
+	atf_set "require.progs" "ggatec"
+	atf_set "require.user" "root"
+	atf_set "timeout" 60
+}
+
+ggatec_trim_body()
+{
+	load_ggate
+
+	us=$(alloc_ggate_dev)
+	work=$(alloc_md)
+	atf_check -e ignore -o ignore dd if=/dev/random of=/dev/$work bs=1m count=1 conv=notrunc
+	echo $CONF >> $PLAINFILES
+	echo "localhost RW /dev/$work" > $CONF
+	atf_check ggated -p $PORT -F $PIDFILE $CONF
+	atf_check ggatec create -p $PORT -u $us localhost /dev/$work
+	ggate_dev=/dev/ggate${us}
+	wait_for_ggate_device ${ggate_dev}
+
+	# ggatec only supports read, write or flush, but not trim (yet).
+	atf_check -s not-exit:0 -e ignore -o ignore trim -q -f ${ggate_dev}
+}
+
+ggatec_trim_cleanup()
+{
+	common_cleanup
+}
+
+
 atf_test_case ggated cleanup
 ggated_head()
 {
@@ -26,10 +101,15 @@ ggated_body()
 	work=$(alloc_md)
 	src=$(alloc_md)
 
+	# we are going to check whether copying data from $src into $work via
+	# $ggate_dev produces the same bytes (checksum).
+
+	# init target device $work (which is going to be wrapped as $ggate_dev)
+	# with garbage that's different from what we have in $src.
 	atf_check -e ignore -o ignore \
-	    dd if=/dev/random of=/dev/$work bs=1m count=1 conv=notrunc
+	    dd if=/dev/random of=/dev/$work bs=1m count=64 conv=notrunc
 	atf_check -e ignore -o ignore \
-	    dd if=/dev/random of=/dev/$src bs=1m count=1 conv=notrunc
+	    dd if=/dev/random of=/dev/$src bs=1m count=64 conv=notrunc
 
 	echo $CONF >> $PLAINFILES
 	echo "127.0.0.1 RW /dev/$work" > $CONF
@@ -41,8 +121,9 @@ ggated_body()
 
 	wait_for_ggate_device ${ggate_dev}
 
+	# copy from $src into $work via $ggate_dev.
 	atf_check -e ignore -o ignore \
-	    dd if=/dev/${src} of=${ggate_dev} bs=1m count=1 conv=notrunc
+	    dd if=/dev/${src} of=${ggate_dev} bs=1m count=64 conv=notrunc
 
 	checksum /dev/$src /dev/$work
 }
@@ -106,9 +187,9 @@ ggatel_md_body()
 	src=$(alloc_md)
 
 	atf_check -e ignore -o ignore \
-	    dd if=/dev/random of=$work bs=1m count=1 conv=notrunc
+	    dd if=/dev/random of=$work bs=1m count=64 conv=notrunc
 	atf_check -e ignore -o ignore \
-	    dd if=/dev/random of=$src bs=1m count=1 conv=notrunc
+	    dd if=/dev/random of=$src bs=1m count=64 conv=notrunc
 
 	atf_check ggatel create -u $us /dev/$work
 
@@ -117,7 +198,7 @@ ggatel_md_body()
 	wait_for_ggate_device ${ggate_dev}
 
 	atf_check -e ignore -o ignore \
-	    dd if=/dev/$src of=${ggate_dev} bs=1m count=1 conv=notrunc
+	    dd if=/dev/$src of=${ggate_dev} bs=1m count=64 conv=notrunc
 
 	checksum /dev/$src /dev/$work
 }
@@ -132,6 +213,8 @@ atf_init_test_cases()
 	atf_add_test_case ggated
 	atf_add_test_case ggatel_file
 	atf_add_test_case ggatel_md
+	atf_add_test_case ggatec_trim
+	atf_add_test_case ggate_flush
 }
 
 alloc_ggate_dev()
@@ -150,7 +233,7 @@ alloc_md()
 {
 	local md
 
-	md=$(mdconfig -a -t malloc -s 1M) || \
+	md=$(mdconfig -a -t malloc -s 64M) || \
 		atf_fail "failed to allocate md device"
 	echo ${md} >> md.devs
 	echo ${md}
